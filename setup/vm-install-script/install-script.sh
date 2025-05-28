a71697bae7314b82f8590b75b609b722c1ab3810
Purushotham <purushotham.muktha@gmail.com>
2025-05-28 15:30:47 +0200

    updated script

--- setup/vm-install-script/install-script.sh
+++ setup/vm-install-script/install-script.sh
@@ -44,9 +44,11 @@ kubeadm init --pod-network-cidr '10.244.0.0/16' --service-cidr '10.96.0.0/16'  -
 mkdir -p ~/.kube
 cp -i /etc/kubernetes/admin.conf ~/.kube/config
 
-kubectl apply -f "https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s-1.11.yaml"
-kubectl rollout status daemonset weave-net -n kube-system --timeout=90s
-sleep 5
+## commented the weave net CNI because its corrupted
+#kubectl apply -f "https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s-1.11.yaml"
+kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+kubectl rollout status daemonset calico-node -n kube-system --timeout=90s
+sleep 10
 
 echo "untaint controlplane node"
 node=$(kubectl get nodes -o=jsonpath='{.items[0].metadata.name}')
@@ -54,6 +56,8 @@ for taint in $(kubectl get node $node -o jsonpath='{range .spec.taints[*]}{.key}
 do
     kubectl taint node $node $taint
 done
+
+swapoff -a
 kubectl get nodes -o wide
 
 echo ".........----------------#################._.-.-Docker-.-._.#################----------------........."
